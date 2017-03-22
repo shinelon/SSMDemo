c55468bde0f0acaaac60599aeb64de0f9d0b51cc
shinelon <syq_sunshine@163.com>
2017-03-22 10:17:03 +0800

    add download

--- src/main/java/demo/controller/fileupload/FileUploadController.java
+++ src/main/java/demo/controller/fileupload/FileUploadController.java
@@ -1,15 +1,22 @@
 package demo.controller.fileupload;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -66,11 +73,13 @@ public class FileUploadController {
     }
 
     @RequestMapping(value = "/basicUploadSave", method = RequestMethod.POST)
-    public void basicUploadSave(MultipartHttpServletRequest request, HttpServletResponse response) {
+    public String basicUploadSave(MultipartHttpServletRequest request, HttpServletResponse response) {
         String path = request.getSession().getServletContext().getRealPath("/");
         System.out.println("path:" + path);
         Map<String, Object> filemap = FileUploadUtil.upload(path + "upload", request);
         System.out.println(filemap);
+        request.setAttribute("filemap", filemap);
+        return "/filedemo/uploadfile";
     }
 
     @RequestMapping(value = "/basicView")
@@ -78,5 +87,48 @@ public class FileUploadController {
         return "/filedemo/basic";
 
     }
+    
+    @RequestMapping(value = "/page")
+    public String page() {
+        return "/filedemo/uploadfile";
 
+    }
+    /**
+     * 文件下载
+     * @Description: 
+     * @param fileName
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException 
+     * @throws FileNotFoundException 
+     */
+    @RequestMapping("/downloadfile")
+    public String downloadFile(@RequestParam("path") String fileName,
+            HttpServletRequest request, HttpServletResponse response){
+        if (fileName != null) {
+            File file = new File(fileName);
+            if (file.exists()) {
+                response.setContentType("application/force-download");// 设置强制下载不打开
+                response.addHeader("Content-Disposition",
+                        "attachment;fileName=" + file.getName());// 设置文件名
+                byte[] buffer = new byte[1024];
+                try(FileInputStream fis = new FileInputStream(file);
+                		BufferedInputStream bis = new BufferedInputStream(fis)) {
+                    
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                } catch(FileNotFoundException e1){
+                	e1.getStackTrace();
+                } catch(IOException e2){
+                	e2.getStackTrace();
+                }
+            }
+        }
+        return null;
+    }
 }
--- src/main/webapp/WEB-INF/views/filedemo/uploadfile.jsp
+++ src/main/webapp/WEB-INF/views/filedemo/uploadfile.jsp
@@ -3,10 +3,19 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>文件上传实例 </title>
 </head>
 <body>
-
-
+<h1>文件上传实例 </h1>
+<form method="post" action="/maven-web-demo/upload/basicUploadSave.htmls" enctype="multipart/form-data">
+    选择一个文件:
+    <input type="file" name="uploadFile" />
+    <br/><br/>
+    <input type="submit" value="上传" />
+</form>
+<br>
+${filemap.fileUrl} 
+<br>
+<a href="/maven-web-demo/upload/downloadfile.htmls?path=${filemap.fileUrl}" >下载</a>  
 </body>
 </html>
